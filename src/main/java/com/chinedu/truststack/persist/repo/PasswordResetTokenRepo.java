b75d05926f0c213e1a269985a1103cc0e3f61bf5
Chinedu Okafor <cokafor@gidimobile.com>
2016-12-08 16:19:22 +0100

    first commit

---
+++ src/main/java/com/chinedu/truststack/persist/repo/PasswordResetTokenRepo.java
@@ -0,0 +1,27 @@
+package com.chinedu.truststack.persist.repo;
+
+import com.chinedu.truststack.persist.entity.PasswordResetToken;
+import com.chinedu.truststack.persist.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.stream.Stream;
+
+/**
+ * Created by cokafor on 11/21/2016.
+ */
+public interface PasswordResetTokenRepo extends JpaRepository<PasswordResetToken, Long> {
+    PasswordResetToken findByToken(String token);
+
+    PasswordResetToken findByUser(User user);
+
+    Stream<PasswordResetToken> findAllByExpiryDateLessThan(Date now);
+
+    void deleteByExpiryDateLessThan(Date now);
+
+    @Modifying
+    @Query("delete from PasswordResetToken t where t.expiryDate <= ?1")
+    void deleteAllExpiredSince(Date now);
+}
